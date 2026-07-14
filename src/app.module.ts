57095f03e6f0699c80d678779d2ac85bb85f7d44
bhkim <xgame1123@naver.com>
2026-07-14 15:13:45 +0900

    프론트 dist 서빙 위해 @nestjs/serve-static 추가
    - ServeStaticModule로 two-step-stats-front/dist 정적 파일 서빙
    - NestJS 10 호환 위해 serve-static 4.0.2 버전 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { StatsModule } from './stats/stats.module';
-
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "./prisma/prisma.module";
+import { StatsModule } from "./stats/stats.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 @Module({
-  imports: [PrismaModule, StatsModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      // 프론트 빌드 결과물(dist)을 API가 같이 내려준다
+      rootPath: join(__dirname, "..", "..", "two-step-stats-front", "dist"),
+    }),
+    PrismaModule,
+    StatsModule,
+  ],
 })
 export class AppModule {}
